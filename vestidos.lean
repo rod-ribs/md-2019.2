d3c0db1b2b76ddc9840eff31dd027dfbb25f9c1a
Alexandre Rademaker <arademaker@gmail.com>
2019-08-16 20:56:55 -0300

    initial version

---
+++ vestidos.lean
@@ -0,0 +1,36 @@
+/-
+Problema dos Vestidos
+ 
+Três irmãs - Ana, Maria e Cláudia - foram a uma festa com vestidos de
+cores diferentes. Uma vestia azul, a outra branco e a Terceira
+preto. Chegando à festa, o anfitrião perguntou quem era cada uma
+delas. As respostas foram:
+
+- A de azul respondeu: “Ana é a que está de branco”
+- A de branco falou: “Eu sou Maria”
+- A de preto disse:  “Cláudia é quem está de branco”
+
+O anfitrião foi capaz de identificar corretamente quem era cada pessoa
+considerando que:
+
+- Ana sempre diz a verdade
+- Maria às vezes diz a verdade
+- Cláudia nunca diz a verdade
+
+Pensando um pouco sobre o problema, pode-se concluir que a Ana estava
+com o vestido preto, a Cláudia com o branco e a Maria com o
+azul.  
+
+Formalizar o problema e usar algum método dedutivo para construir um
+argumento formal a favor da conclusão. 
+
+Dica: A tabela verdade teria 512 linhas! Existem várias possiveis
+formalizações, na que estamos interesados, todos os simbolos
+proposicionais necessários estão declarados abaixo. 
+
+Vc deve interpretar as respostas e conclusões do anfitrião
+considerando as possibilidades de quem poderia ter falado o que.
+
+-/
+
+variables AA AB AP MA MB MP CA CB CP : Prop 
